3082004bd5e5a6f9eab9289ff64402adaca212de
AAKorovichev <AAKorovichev@mai.education>
2024-03-30 11:58:44 +0300

    initial commit

---
+++ server.cpp
@@ -0,0 +1,14 @@
+#include "./headers/server.h"
+#include <httplib.h>
+
+RestServer::RestServer() {}
+
+void RestServer::start() {
+    httplib::Server svr;
+
+    svr.Get("/hi", [](const httplib::Request &, httplib::Response &res) {
+        res.set_content("Hello World!", "text/plain");
+    });
+
+    svr.listen(hostname, port);
+}
---
+++ server.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+class RestServer {
+    std::string hostname = "0.0.0.0";
+    int port = 8080;
+    
+    public:
+        RestServer();
+        void start();
+};
